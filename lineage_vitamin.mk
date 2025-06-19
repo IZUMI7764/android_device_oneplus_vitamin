fa376007bc324b288b6765c7e5c9abaadc58c82f
WuXing90 <2096477910@qq.com>
2025-06-19 14:45:43 +0800

    Vitamin: Update blobs from PHP110_15.0.0.901(CN01)

--- lineage_vitamin.mk
+++ lineage_vitamin.mk
@@ -12,15 +12,15 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_BRAND := OnePlus
 PRODUCT_DEVICE := vitamin
 PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_MODEL := CPH2493
+PRODUCT_MODEL := PHP110
 PRODUCT_NAME := lineage_vitamin
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="sys_mssi_64_cn_armv82-user 15 AP3A.240617.008 1742885549055 release-keys" \
-    BuildFingerprint=OnePlus/CPH2493EEA/OP556FL1:15/AP3A.240617.008/T.R4T3.1d25f69-14c1e-114e5:user/release-keys \
-    DeviceName=OP556FL1 \
-    DeviceProduct=CPH2493EEA \
-    SystemDevice=OP556FL1 \
-    SystemName=CPH2493EEA
+    BuildDesc="sys_mssi_64_cn_armv82-user 15 AP3A.240617.008 1747034835004 release-keys" \
+    BuildFingerprint=OnePlus/PHP110/OP5927:15/AP3A.240617.008/T.1dc961e-e286-5e73d:user/release-keys \
+    DeviceName=OP5927 \
+    DeviceProduct=PHP110 \
+    SystemDevice=OP5927 \
+    SystemName=PHP110
