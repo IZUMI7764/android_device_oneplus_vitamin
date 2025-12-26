1cf39afec040b4a901389e3a5ea0c54ff80a069c
WuXing90 <2096477910@qq.com>
2025-12-27 03:14:55 +0800

    vitamin: Import device-specific sepolicy
    Migrate the device-specific sepolicy from our legacy  hardware/oplus  directory to the device tree
    
    Signed-off-by: wuxinG90 <2096477910df@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,9 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
-include hardware/oplus/sepolicy/mtk/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Verified boot
 BOARD_AVB_ENABLE := true
