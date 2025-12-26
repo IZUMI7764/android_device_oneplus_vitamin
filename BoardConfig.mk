c33946d1a4734da0fc42e0d54e5365fed6a42068
WuXing90 <2096477910@qq.com>
2025-12-27 03:00:46 +0800

    vitamin: Import lineage device_framework_matrix
    Since some custom ROMs have deprecated this matrix, while our device still requires it, we are importing this matrix file into the device tree
    
    Signed-off-by: wuxinG90 <2096477910df@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_SCREEN_DENSITY := 560
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     hardware/oplus/vintf/device_framework_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(DEVICE_PATH)/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 
