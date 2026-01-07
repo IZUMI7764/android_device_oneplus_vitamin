508cfd498245c8d1c13649f903549cf7cd1d9de4
NINJAHUNTER1357 <adityasingh6901012a@gmail.com>
2026-01-07 07:46:07 +0000

    vitamin: Patch libsink-mtk.so with correct libshim
    Signed-off-by: wuxinG90 <2096477910df@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -52,7 +52,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
     'system_ext/lib64/libsink-mtk.so': blob_fixup()
-        .add_needed('libaudioclient_shim.so'),
+        .add_needed('libaudioclient_mt6983_shim.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
     (
