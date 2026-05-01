04c5f89e2ba6c9c5a10bca84aa288e146cbf2565
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-05-01 12:46:16 +0000

    vitamin: fixup! extract-files.py
    Signed-off-by: Adarsh0127-Elite <adarshmoharana027@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -29,27 +29,20 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
 }
 
-
 blob_fixups: blob_fixups_user_type = {
-    (
-        'odm/lib64/libPanelChaplin_mtk.so',
-        'vendor/lib64/mt6983/libmtkcam_stdutils.so'
-    ): blob_fixup()
+    ('odm/lib64/libPanelChaplin_mtk.so', 'vendor/lib64/mt6983/libmtkcam_stdutils.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
     'odm/lib64/vendor.oplus.hardware.hdcp-V1-ndk_platform.so': blob_fixup()
         .replace_needed('android.hardware.common-V2-ndk_platform.so', 'android.hardware.common-V2-ndk.so'),
-    (
-        'vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc'
-    ): blob_fixup()
+        'vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc': blob_fixup()
         .regex_replace('start', 'enable'),
+    'system_ext/lib64/libimsma.so': blob_fixup()
+        .replace_needed('libsink.so', 'libsink-mtk.so'),
     'system_ext/lib64/libsink-mtk.so': blob_fixup()
         .add_needed('libaudioclient_mt6983_shim.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
-    (
-        'vendor/bin/hw/android.hardware.gnss-service.mediatek',
-        'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'
-    ): blob_fixup()
+    ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
@@ -59,22 +52,16 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so'),
-    (
-        'vendor/bin/mnld',
-        'vendor/lib64/liboplus_mtkcam_lightsensorprovider.so',
-        'vendor/lib64/mt6983/libaalservice.so'
-    ): blob_fixup()
+    ('vendor/bin/mnld', 'vendor/lib64/liboplus_mtkcam_lightsensorprovider.so', 'vendor/lib64/mt6983/libaalservice.so'): blob_fixup()
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libalsautils.so', 'libalsautils-v31.so'),
-    (
-        'vendor/lib64/hw/hwcomposer.mtk_common.so',
+    ('vendor/lib64/hw/hwcomposer.mtk_common.so',
         'vendor/lib64/mt6983/libcam.hal3a.so',
         'vendor/lib64/mt6983/libcam.hal3a.ctrl.so',
         'vendor/lib64/mt6983/libmtkcam_request_requlator.so',
-        'vendor/lib64/libcustomer_cameradata.so'
-    ): blob_fixup()
+        'vendor/lib64/libcustomer_cameradata.so'): blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/hw/mt6983/android.hardware.camera.provider@2.6-impl-mediatek.so': blob_fixup()
         .add_needed('libcamera_metadata_shim.so')
@@ -84,13 +71,7 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'/my_product/vendor/etc/cust_silky_brightness_%s_%s.xml', b'/vendor/etc/cust_silky_brightness_%s_%s.xml\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
         .binary_regex_replace(b'/my_product/vendor/etc/cust_silky_brightness_%s.xml', b'/vendor/etc/cust_silky_brightness_%s.xml\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
         .replace_needed('libutils.so', 'libutils-v32.so'),
-    (
-        'vendor/lib64/mt6983/lib3a.awbsync.so',
-        'vendor/lib64/mt6983/lib3a.flash.so',
-        'vendor/lib64/mt6983/lib3a.sensors.color.so',
-        'vendor/lib64/mt6983/lib3a.sensors.flicker.so',
-        'vendor/lib64/lib3a.ae.pipe.so'
-    ): blob_fixup()
+    ('vendor/lib64/mt6983/lib3a.awbsync.so', 'vendor/lib64/mt6983/lib3a.flash.so', 'vendor/lib64/mt6983/lib3a.sensors.color.so', 'vendor/lib64/mt6983/lib3a.sensors.flicker.so', 'vendor/lib64/lib3a.ae.pipe.so'): blob_fixup()
         .add_needed('liblog.so'),
     'vendor/lib64/mt6983/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
@@ -105,20 +86,11 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcutils.so'),
     'vendor/lib64/libmidasserviceintf_aidl.so': blob_fixup()
         .replace_needed('android.frameworks.stats-V1-ndk_platform.so', 'android.frameworks.stats-V1-ndk.so'),
-    (
-        'vendor/lib64/libnvram.so',
-        'vendor/lib64/libsysenv.so'
-    ): blob_fixup()
+    ('vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
         .add_needed('libbase_shim.so'),
         'vendor/bin/hw/mtkfusionrild': blob_fixup()
         .add_needed('libutils-v32.so'),
-    (
-        'vendor/oneplus/vitamin/proprietary/odm/bin/touchDaemon',
-        'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff',
-        'vendor/lib64/hw/audio.primary.mediatek.so',
-        'vendor/lib64/librt_extamp_intf.so',
-        'vendor/lib64/hw/mt6983/vendor.mediatek.hardware.pq@2.15-impl.so',
-    ): blob_fixup()
+    ('vendor/oneplus/vitamin/proprietary/odm/bin/touchDaemon', 'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff', 'vendor/lib64/hw/audio.primary.mediatek.so', 'vendor/lib64/librt_extamp_intf.so', 'vendor/lib64/hw/mt6983/vendor.mediatek.hardware.pq@2.15-impl.so',): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
