b930fde371067b1f2ec6a11166108e645d4976f0
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-05-01 10:40:04 +0000

    vitamin: Build mediatek-common JAR from source
    Signed-off-by: Adarsh0127-Elite <adarshmoharana027@gmail.com>

--- device.mk
+++ device.mk
@@ -166,6 +166,7 @@ PRODUCT_PACKAGES += \
 ENABLE_VENDOR_RIL_SERVICE := true
 
 $(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
+$(call inherit-product, hardware/mediatek/frameworks/mediatek-frameworks.mk)
 
 $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
 
