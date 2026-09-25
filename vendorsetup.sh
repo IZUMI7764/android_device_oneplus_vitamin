dbddd93be7b2076750777f18bda26042a69f2283
IZUMI <124417505+IZUMI7764@users.noreply.github.com>
2026-09-25 20:04:23 +0530

    Add ViPER4AndroidFX to vendorsetup.sh
    Cloned ViPER4AndroidFX repository into packages/apps.
    
    Signed-off-by: IZUMI <124417505+IZUMI7764@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,9 @@ cd packages/apps/Aperture
 git fetch https://github.com/Adarsh0127-Elite/android_packages_apps_Aperture.git
 git cherry-pick 9509277efc852ad8bdcce204e0d9cfe104b6d190
 cd ../../..
+
+echo "Cloning Viper4FX"
+mkdir -p packages/apps
+cd packages/apps
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git ViPER4AndroidFX
+cd ../..
